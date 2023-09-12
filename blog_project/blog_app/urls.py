e01b255fa39e0e517ef99bf92d584af4203da426
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2023-09-13 00:09:58 +0900

    modify urls.py
    modify login urls

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.RegisterAPIView.as_view()),
-    path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
