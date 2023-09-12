fb9b03a781bff8f05748e6f952e69a8653291f4f
samkimpepper <vegielcl@gmail.com>
2023-09-12 17:20:20 +0900

    [modify] 템플릿과 연결

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login, name='login_view', methods=['get']),
+    path('register/', views.RegisterAPIView.as_view()),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
