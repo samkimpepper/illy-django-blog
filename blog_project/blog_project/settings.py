ecb51fa3eb4c397873151984d0309c7ca7f3a242
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2023-09-13 14:57:54 +0900

    modifysettings.py
    modify login

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 로그인 성공후 이동하는 URL
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/board.html/'
 
 # 로그아웃시 이동하는 URL
 LOGOUT_REDIRECT_URL = '/'
