fbf29451493bbe457978289ab087fb2b19e358e7
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2023-09-13 14:53:02 +0900

    modift views.py
    modify login

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import View
+from django.http import JsonResponse
+from django.conf import settings
+from .forms import CustomLoginForm
+
+from rest_framework import generics
+
 
 from rest_framework.views import APIView
 
@@ -27,3 +35,20 @@ class LoginAPIView(APIView):
         
         return redirect('blog/postlist-admin.html')
 
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('blog_app:post_list')
+    else:
+        form = CustomLoginForm(data=request.POST or None)  # 커스텀 로그인 폼 사용
+        if request.method == "POST":
+
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                    login(request, user)
+                    return redirect('blog_app:post_list')  # 슈퍼유저와 일반 사용자 모두 동일한 페이지로 리다이렉션
+        return render(request, 'blog/login.html', {'form': form})
