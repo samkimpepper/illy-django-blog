18ea7565fe536d5c43a6286d926bb77aedbfdddf
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2023-09-14 17:03:29 +0900

    modify models.py

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=128)
-    
+
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     published_at = models.DateTimeField(auto_now_add=True)
-    # views : 수정필요, 
+    created_at = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0)
     topic = models.CharField(max_length=255, default='전체')
     img = models.ImageField(upload_to="images/")
@@ -23,6 +23,5 @@ class Article(models.Model):
         return self.title
     
     def save(self, *args, **kwargs):
-        # '..' 문자열이 포함된 content 필드를 변경
         self.content = self.content.replace('"..', '"')
         super().save(*args, **kwargs)
