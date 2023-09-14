2cd5a297b7e3572edd7e97e85fb596cfd34ef1aa
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2023-09-14 14:39:05 +0900

    modify models.py
    modify

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -11,27 +11,19 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    publish = models.CharField(max_length=1, default='Y')
     published_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     # views : 수정필요, 
     views = models.IntegerField(default=0)
     topic = models.CharField(max_length=255, default='전체')
     img = models.ImageField(upload_to="images/")
-
-
-class BlogPost(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    topic = models.CharField(max_length=255, default='전체')
     publish = models.CharField(max_length=1, default='Y')
-    views = models.IntegerField(default=0)
     author_id = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.title
     
     def save(self, *args, **kwargs):
+        # '..' 문자열이 포함된 content 필드를 변경
         self.content = self.content.replace('"..', '"')
         super().save(*args, **kwargs)
-
