143da9747c8fef5336b6ae9b0b96c3d2833307f6
jun-kyeong <ojk27974992@gmail.com>
2023-09-14 09:27:15 +0900

    [fix] 메인 머지에서 누락된것 추가 -by 오준경

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -2,10 +2,13 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views 
 
-app_name = 'users'
+app_name = 'blog'
 
 urlpatterns = [
     path('register/', views.RegisterAPIView.as_view()),
-    path('login/', auth_views.LoginView.as_view(template_name='blog_app/login.html'), name='login'),
+    # path('login/', auth_views.LoginView.as_view(template_name='blog_app/login.html'), name='login'),
+    path('login/',views.login_Form,name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('board_client',views.board_client,name='board_client'),
+    path('board_admin',views.board_admin,name='board_client')
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.views import View
-from django.http import JsonResponse
-from django.conf import settings
-from .forms import CustomLoginForm
-from .forms import LoginForm
-
-from rest_framework import generics
-
-
 from rest_framework.views import APIView
-
 from .models import *
 from .serializers import *
-
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 class RegisterAPIView(APIView):
     def get(self, request):
@@ -36,29 +26,15 @@ class RegisterAPIView(APIView):
         
 #         return redirect('blog/postlist-admin.html')
 
+
 #board_client 페이지 렌더링
 def board_client(request):
     return render(request, 'blog_app/board_client.html')
-    
 
-# def custom_login(request):
-#     if request.user.is_authenticated:
-#         return redirect('blog_app:post_list')
-#     else:
-#         form = CustomLoginForm(data=request.POST or None)  # 커스텀 로그인 폼 사용
-#         if request.method == "POST":
+#board_admin 페이지 렌더링
+def board_admin(request):
+    return render(request, 'blog_app/board_admin.html')
 
-#             if form.is_valid():
-#                 username = form.cleaned_data['username']
-#                 password = form.cleaned_data['password']
-
-#                 user = authenticate(request, username=username, password=password)
-
-#                 if user is not None:
-#                     login(request, user)
-#                     return redirect('blog_app:post_list')  # 슈퍼유저와 일반 사용자 모두 동일한 페이지로 리다이렉션
-#         return render(request, '/blog_app/login.html', {'form': form})
-        
 
 #Form으로 로그인 화면 렌더링
 def login_Form(request):
@@ -76,15 +52,3 @@ def login_Form(request):
         form = LoginForm()
     
     return render(request, 'blog_app/login.html', {'form': form})
-    
-# def write(request):
-#     if request.method == 'POST':
-#         form = ArticleForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('blog_app/board.html')
-        
-#     else:
-#         form = ArticleForm()
-
-#     return render(request, 'blog_app/write.html', {'form': form})
